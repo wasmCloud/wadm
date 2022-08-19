e91246f2c09a72730c2cc59dbc1c82bd2b7dae46
Brooks Townsend <brooks@cosmonic.com>
2022-08-19 14:49:34 -0400

    updated cloudevents to 0.6.1
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- wadm/mix.exs
+++ wadm/mix.exs
@@ -4,7 +4,7 @@ defmodule Wadm.MixProject do
   def project do
     [
       app: :wadm,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: compiler_paths(Mix.env()),
@@ -25,18 +25,18 @@ defmodule Wadm.MixProject do
   defp deps do
     [
       {:yaml_elixir, "~> 2.8"},
-      {:cloudevents, "~> 0.4.0"},
+      {:cloudevents, "~> 0.6.1"},
       {:uuid, "~> 1.1"},
       {:libcluster, "~> 3.3"},
       {:horde, "~> 0.8.7"},
       {:gnat, "~> 1.4"},
       {:redix, "~> 1.1"},
-      {:erlavro, "~> 2.9", manager: :rebar3, override: true},
       {:phoenix_pubsub, "~> 2.1"},
       {:libvault, "~> 0.2.3"},
       {:tesla, "~> 1.4.4"},
       {:hackney, "~> 1.17"},
-      {:lattice_observer, git: "https://github.com/wasmcloud/lattice-observer"},
+      {:lattice_observer,
+       git: "https://github.com/wasmcloud/lattice-observer", branch: "fix/cloudevents-noavro"},
       {:vapor, git: "https://github.com/autodidaddict/vapor"}
     ]
   end
