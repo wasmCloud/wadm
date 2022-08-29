9fc546113eacd35faf6d737f037cef1472c306db
Brooks Townsend <brooks@cosmonic.com>
2022-08-29 14:28:00 -0400

    Default to info level logs in prod (#34)
    * default to info level logs in prod
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * import config
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * removed debug logs from gnat
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- wadm/config/config.exs
+++ wadm/config/config.exs
@@ -1,5 +1,11 @@
 import Config
 
-config :logger, :console, format: "$time $metadata[$level] $message\n"
+config :logger,
+  compile_time_purge_matching: [
+    [application: :gnat],
+    [level_lower_than: :info]
+  ]
+
+config :logger, :console, format: "$time $metadata[$level] $message\n", level: :debug
 
 import_config "#{config_env()}.exs"
