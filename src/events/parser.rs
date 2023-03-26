6a4f2fbd35b4c9a5db0c03e0a7cbc9668b80e827
aish-where-ya <aharpale@cosmonic.com>
2023-03-26 16:48:36 -0400

    Addressed review comments
    Signed-off-by: aish-where-ya <aharpale@cosmonic.com>

--- src/events/parser.rs
+++ src/events/parser.rs
@@ -1,28 +1,27 @@
-use anyhow::Result;
-use serde_json;
-use serde_yaml;
 use std::collections::BTreeMap;
-use std::io::BufReader;
-use std::path::PathBuf;
 
 use serde::{Deserialize, Serialize};
 
+pub const DEFAULT_WEIGHT: i32 = 100;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ComponentType {
-    actor,
-    capability,
+    #[serde(rename = "actor")]
+    Actor,
+    #[serde(rename = "capability")]
+    Capability,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Annotations {
-    version: String,
-    description: String,
+    pub version: String,
+    pub description: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Metadata {
-    name: String,
-    annotations: Annotations,
+    pub name: String,
+    pub annotations: Annotations,
 }
 
 fn default_link_name() -> Option<String> {
@@ -31,118 +30,128 @@ fn default_link_name() -> Option<String> {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Properties {
-    image: String,
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    contract: Option<String>,
+    pub image: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub contract: Option<String>,
     #[serde(default = "default_link_name", skip_serializing_if = "Option::is_none")]
-    link_name: Option<String>,
+    pub link_name: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum TraitType {
-    spreadscaler,
-    linkdef,
-}
-#[derive(Debug, Serialize, Deserialize)]
-pub enum SpreadRequirements {
-    app(String),
-    zone(String),
-    ledenabled(bool),
+    #[serde(rename = "spreadscaler")]
+    Spreadscaler,
+    #[serde(rename = "linkdef")]
+    Linkdef,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum LinkdefValueItems {
-    uri(String),
-    address(String),
-    port(i32),
+pub struct LinkdefValueItems {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub uri: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub address: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub port: Option<i32>,
 }
 
 fn default_weight() -> Option<i32> {
-    Some(100)
+    Some(DEFAULT_WEIGHT)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum SpreadRequirementValues {
     String(String),
-    bool(bool),
-    i32(i32),
-    char(char),
+    Bool(bool),
+    I32(i32),
+    Char(char),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Spread {
-    name: String,
-    requirements: BTreeMap<String, SpreadRequirementValues>,
+    pub name: String,
+    pub requirements: BTreeMap<String, SpreadRequirementValues>,
     #[serde(default = "default_weight", skip_serializing_if = "Option::is_none")]
-    weight: Option<i32>,
+    pub weight: Option<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LinkdefProperty {
+    pub target: String,
+    pub values: LinkdefValueItems,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SpreadScalerProperty {
+    pub replicas: i32,
+    pub spread: Vec<Spread>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum TraitPropertyItem {
-    LinkdefProperty {
-        target: String,
-        values: LinkdefValueItems,
-    },
-    SpreadScalerProperty {
-        replicas: i32,
-        spread: Vec<Spread>,
-    },
+    LinkdefProperty(LinkdefProperty),
+    SpreadScalerProperty(SpreadScalerProperty),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Traits {
     #[serde(rename = "type")]
-    trait_type: TraitType,
-    properties: TraitPropertyItem,
+    pub trait_type: TraitType,
+    pub properties: TraitPropertyItem,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ComponentItem {
-    name: String,
+    pub name: String,
     #[serde(rename = "type")]
-    component_type: ComponentType,
-    properties: Properties,
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    traits: Option<Vec<Traits>>,
+    pub component_type: ComponentType,
+    pub properties: Properties,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub traits: Option<Vec<Traits>>,
 }
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Components {
-    components: Vec<ComponentItem>,
+    pub components: Vec<ComponentItem>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Manifest {
-    apiVersion: String,
-    kind: String,
-    metadata: Metadata,
-    spec: Components,
-}
-
-pub(crate) fn deserialize_yaml(filepath: PathBuf) -> Result<Manifest> {
-    let file = std::fs::File::open(filepath)?;
-    let reader = BufReader::new(file);
-    let yaml_string: Manifest = serde_yaml::from_reader(reader)?;
-    println!("Read YAML string: {:?}", yaml_string);
-    Ok(yaml_string)
-}
-
-pub(crate) fn deserialize_json(filepath: PathBuf) -> Result<Manifest> {
-    let file = std::fs::File::open(filepath)?;
-    let reader = BufReader::new(file);
-    let json_string: Manifest = serde_json::from_reader(reader)?;
-    println!("Read JSON string: {:?}", json_string);
-    Ok(json_string)
+    #[serde(rename = "apiVersion")]
+    pub api_version: String,
+    pub kind: String,
+    pub metadata: Metadata,
+    pub spec: Components,
 }
 
 #[cfg(test)]
 mod test {
 
     use super::*;
+    use anyhow::Result;
+    use serde_json;
+    use serde_yaml;
+    use std::io::BufReader;
+    use std::path::PathBuf;
+    pub(crate) fn deserialize_yaml(filepath: PathBuf) -> Result<Manifest> {
+        let file = std::fs::File::open(filepath)?;
+        let reader = BufReader::new(file);
+        let yaml_string: Manifest = serde_yaml::from_reader(reader)?;
+        println!("Read YAML string: {:?}", yaml_string);
+        Ok(yaml_string)
+    }
+
+    pub(crate) fn deserialize_json(filepath: PathBuf) -> Result<Manifest> {
+        let file = std::fs::File::open(filepath)?;
+        let reader = BufReader::new(file);
+        let json_string: Manifest = serde_json::from_reader(reader)?;
+        println!("Read JSON string: {:?}", json_string);
+        Ok(json_string)
+    }
 
     #[test]
-    fn test_oam_desrializer() {
+    fn test_oam_deserializer() {
         let filepath = PathBuf::from("./oam/simple1.json");
         let res = deserialize_json(filepath);
         match res {
@@ -159,8 +168,8 @@ mod test {
     }
     #[test]
     fn test_oam_serializer() {
-        let mut spreadVec: Vec<Spread> = Vec::new();
-        let spreadItem = Spread {
+        let mut spread_vec: Vec<Spread> = Vec::new();
+        let spread_item = Spread {
             name: "eastcoast".to_string(),
             requirements: BTreeMap::from([(
                 "zone".to_string(),
@@ -168,8 +177,8 @@ mod test {
             )]),
             weight: Some(80),
         };
-        spreadVec.push(spreadItem);
-        let spreadItem = Spread {
+        spread_vec.push(spread_item);
+        let spread_item = Spread {
             name: "westcoast".to_string(),
             requirements: BTreeMap::from([(
                 "zone".to_string(),
@@ -177,39 +186,45 @@ mod test {
             )]),
             weight: Some(20),
         };
-        spreadVec.push(spreadItem);
-        let mut traitVec: Vec<Traits> = Vec::new();
-        let traitItem = Traits {
-            trait_type: TraitType::spreadscaler,
-            properties: TraitPropertyItem::SpreadScalerProperty {
-                replicas: 4,
-                spread: spreadVec,
+        spread_vec.push(spread_item);
+        let mut trait_vec: Vec<Traits> = Vec::new();
+        let spreadscalerprop = SpreadScalerProperty {
+            replicas: 4,
+            spread: spread_vec,
+        };
+        let trait_item = Traits {
+            trait_type: TraitType::Spreadscaler,
+            properties: TraitPropertyItem::SpreadScalerProperty(spreadscalerprop),
+        };
+        trait_vec.push(trait_item);
+        let linkdefprop = LinkdefProperty {
+            target: "webcap".to_string(),
+            values: LinkdefValueItems {
+                uri: None,
+                address: None,
+                port: Some(4000),
             },
         };
-        traitVec.push(traitItem);
-        let traitItem = Traits {
-            trait_type: TraitType::linkdef,
-            properties: TraitPropertyItem::LinkdefProperty {
-                target: "webcap".to_string(),
-                values: LinkdefValueItems::port(4000),
-            },
+        let trait_item = Traits {
+            trait_type: TraitType::Linkdef,
+            properties: TraitPropertyItem::LinkdefProperty(linkdefprop),
         };
-        traitVec.push(traitItem);
-        let mut componentVec: Vec<ComponentItem> = Vec::new();
-        let componentItem = ComponentItem {
+        trait_vec.push(trait_item);
+        let mut component_vec: Vec<ComponentItem> = Vec::new();
+        let component_item = ComponentItem {
             name: "userinfo".to_string(),
-            component_type: ComponentType::actor,
+            component_type: ComponentType::Actor,
             properties: Properties {
                 image: "wasmcloud.azurecr.io/fake:1".to_string(),
                 contract: None,
                 link_name: None,
             },
-            traits: Some(traitVec),
+            traits: Some(trait_vec),
         };
-        componentVec.push(componentItem);
-        let componentItem = ComponentItem {
+        component_vec.push(component_item);
+        let component_item = ComponentItem {
             name: "webcap".to_string(),
-            component_type: ComponentType::capability,
+            component_type: ComponentType::Capability,
             properties: Properties {
                 image: "wasmcloud.azurecr.io/httpserver:0.13.1".to_string(),
                 contract: Some("wasmcloud:httpserver".to_string()),
@@ -217,41 +232,42 @@ mod test {
             },
             traits: None,
         };
-        componentVec.push(componentItem);
+        component_vec.push(component_item);
 
-        let mut spreadVec: Vec<Spread> = Vec::new();
-        let spreadItem = Spread {
+        let mut spread_vec: Vec<Spread> = Vec::new();
+        let spread_item = Spread {
             name: "haslights".to_string(),
             requirements: BTreeMap::from([(
                 "zone".to_string(),
-                SpreadRequirementValues::bool(true),
+                SpreadRequirementValues::Bool(true),
             )]),
             weight: default_weight(),
         };
-        spreadVec.push(spreadItem);
-        let mut traitVec: Vec<Traits> = Vec::new();
-        let traitItem = Traits {
-            trait_type: TraitType::spreadscaler,
-            properties: TraitPropertyItem::SpreadScalerProperty {
-                replicas: 1,
-                spread: spreadVec,
-            },
+        spread_vec.push(spread_item);
+        let spreadscalerprop = SpreadScalerProperty {
+            replicas: 1,
+            spread: spread_vec,
         };
-        traitVec.push(traitItem);
-        let componentItem = ComponentItem {
+        let mut trait_vec: Vec<Traits> = Vec::new();
+        let trait_item = Traits {
+            trait_type: TraitType::Spreadscaler,
+            properties: TraitPropertyItem::SpreadScalerProperty(spreadscalerprop),
+        };
+        trait_vec.push(trait_item);
+        let component_item = ComponentItem {
             name: "ledblinky".to_string(),
-            component_type: ComponentType::capability,
+            component_type: ComponentType::Capability,
             properties: Properties {
                 image: "wasmcloud.azurecr.io/ledblinky:0.0.1".to_string(),
                 contract: Some("wasmcloud:blinkenlights".to_string()),
                 link_name: default_link_name(),
             },
-            traits: Some(traitVec),
+            traits: Some(trait_vec),
         };
-        componentVec.push(componentItem);
+        component_vec.push(component_item);
 
         let spec = Components {
-            components: componentVec,
+            components: component_vec,
         };
         let metadata = Metadata {
             name: "my-example-app".to_string(),
@@ -261,17 +277,19 @@ mod test {
             },
         };
         let manifest = Manifest {
-            apiVersion: "core.oam.dev/v1beta1".to_string(),
+            api_version: "core.oam.dev/v1beta1".to_string(),
             kind: "application".to_string(),
-            metadata: metadata,
-            spec: spec,
+            metadata,
+            spec,
         };
         let serialized_json = serde_json::to_string(&manifest);
+
         match serialized_json {
             Ok(parse_results) => parse_results,
             Err(error) => panic!("Error {:?}", error),
         };
         let serialized_yaml = serde_yaml::to_string(&manifest);
+
         match serialized_yaml {
             Ok(parse_results) => parse_results,
             Err(error) => panic!("Error {:?}", error),
