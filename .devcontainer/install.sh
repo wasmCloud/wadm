b2690718588ab051a44de4169b3a2f4b66e632d3
Brooks Townsend <brooks@cosmonic.com>
2023-04-17 23:48:39 +0000

    updated to use alpha tag
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo install wash-cli --git https://github.com/wasmcloud/wash --branch feat/wadm_0.4_support --force 
-cargo install wadm --bin wadm --features cli --git https://github.com/wasmcloud/wadm --branch wadm_0.4 --force
+cargo install wadm --bin wadm --features cli --git https://github.com/wasmcloud/wadm --tag v0.4.0-alpha.1 --force
