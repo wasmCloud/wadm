1ad6bf756acdf60cd3a9a9a4d8c194ca843aa5f6
Brooks Townsend <brooks@cosmonic.com>
2023-04-18 00:37:34 +0000

    updated devcontainer to pull from release artifact
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+# INSTALL WADM
+apt update; apt install curl -y
+
+ARCH=$(arch)
+VERSION=v0.4.0-alpha.1
+
+if [[ $ARCH == "arm64" ]]; then
+    ARCH="aarch64"
+fi
+if [[ $ARCH == "x86_64" ]]; then
+    ARCH="amd64"
+fi
+
+TARBALL=wadm-$VERSION-linux-$ARCH.tar.gz
+
+curl -fLO https://github.com/wasmCloud/wadm/releases/download/$VERSION/$TARBALL
+tar -xvf $TARBALL
+mv $TARBALL/wadm /usr/local/bin/wadm
+
+# INSTALL WASH
 cargo install wash-cli --git https://github.com/wasmcloud/wash --branch feat/wadm_0.4_support --force 
-cargo install wadm --bin wadm --features cli --git https://github.com/wasmcloud/wadm --tag v0.4.0-alpha.1 --force
