188a2682b5304537b071eb04db7533e7489897ec
Brooks Townsend <brooks@cosmonic.com>
2023-04-18 08:24:58 +0200

    container tweaks, updated README to pull release
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # INSTALL WADM
-apt update; apt install curl -y
-
 ARCH=$(arch)
 VERSION=v0.4.0-alpha.1
 
-if [[ $ARCH == "arm64" ]]; then
-    ARCH="aarch64"
-fi
 if [[ $ARCH == "x86_64" ]]; then
     ARCH="amd64"
 fi
@@ -16,7 +11,7 @@ fi
 TARBALL=wadm-$VERSION-linux-$ARCH
 
 curl -fLO https://github.com/wasmCloud/wadm/releases/download/$VERSION/$TARBALL.tar.gz
-tar -xvf $TARBALL
+tar -xvf $TARBALL.tar.gz
 mv $TARBALL/wadm /usr/local/bin/wadm
 rm -rf $TARBALL $TARBALL.tar.gz
 
