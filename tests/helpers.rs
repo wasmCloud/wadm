cd87cd3564e7dbc43c88f23b93bf2e6f70a12b8f
stephenandary <stephen@jamminmusic.com>
2023-04-19 15:42:20 -0400

    WIP - Initial Commit - Making sure Wash Host goes down after each test since it is being started on every test that requires it

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -132,6 +132,22 @@ async fn start_wash_instance(cfg: &TestWashConfig) -> Result<CleanupGuard> {
     Ok(guard)
 }
 
+async fn stop_wash_instance() -> Result<CleanupGuard> {
+    let mut command = tokio::process::Command::new("wash");
+    command.arg("down");
+
+    // Give the host time to totally end
+    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+
+    // Build the cleanup guard that will be returned
+    let guard = CleanupGuard {
+        child: None,
+        already_running: false,
+    };
+
+    Ok(guard)
+}
+
 /// Set up and run a wash instance that can be used for a test
 pub async fn setup_test_wash(cfg: &TestWashConfig) -> CleanupGuard {
     match tokio::net::TcpStream::connect(cfg.washboard_url()).await {
