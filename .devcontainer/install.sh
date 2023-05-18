f4234295f65c7c47ffb6cd72c3c80d4bd39d10bb
Brooks Townsend <brooks@cosmonic.com>
2023-05-18 11:50:48 -0400

    updated devcontainer and install instructions
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    install things
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
 # INSTALL WASH
-cargo install wash-cli --git https://github.com/wasmcloud/wash --branch feat/wadm_0.4_support --force 
-
-# INSTALL WADM
-ARCH=$(arch)
-VERSION=v0.4.0-alpha.1
-
-if [[ $ARCH == "x86_64" ]]; then
-    ARCH="amd64"
-fi
-
-TARBALL=wadm-$VERSION-linux-$ARCH
-
-curl -fLO https://github.com/wasmCloud/wadm/releases/download/$VERSION/$TARBALL.tar.gz
-tar -xvf $TARBALL.tar.gz
-chmod +x $TARBALL/wadm
-mv $TARBALL/wadm /usr/local/cargo/bin/wadm
-rm -rf $TARBALL $TARBALL.tar.gz
+curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | sudo bash
+sudo apt install wash openssl -y
