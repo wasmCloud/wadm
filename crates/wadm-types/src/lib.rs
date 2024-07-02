0df0db08783e1bb1191e98f26ded64ad00e9ec5e
luk3ark <luk3ark@gmail.com>
2024-07-01 20:14:03 -0400

    finalized
    Signed-off-by: luk3ark <luk3ark@gmail.com>

--- crates/wadm-types/src/lib.rs
+++ crates/wadm-types/src/lib.rs
@@ -3,11 +3,10 @@ use std::collections::{BTreeMap, HashMap};
 use serde::{Deserialize, Serialize};
 
 pub mod api;
-#[cfg(target_family = "wasm")]
-mod bindings;
-#[cfg(target_family = "wasm")]
+pub mod bindings;
 pub use bindings::*;
 pub mod validation;
+
 /// The default weight for a spread
 pub const DEFAULT_SPREAD_WEIGHT: usize = 100;
 /// The expected OAM api version
