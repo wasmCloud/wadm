e2764c720bb100af9bed72fca4202f7b90bfe8a4
Brooks Townsend <brooksmtownsend@gmail.com>
2024-08-06 10:59:42 -0400

    fix(bindings): add waiting status
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/wadm-types/src/bindings.rs
+++ crates/wadm-types/src/bindings.rs
@@ -257,6 +257,7 @@ impl From<StatusType> for wadm::types::StatusType {
             StatusType::Reconciling => wadm::types::StatusType::Reconciling,
             StatusType::Deployed => wadm::types::StatusType::Deployed,
             StatusType::Failed => wadm::types::StatusType::Failed,
+            StatusType::Waiting => wadm::types::StatusType::Waiting,
         }
     }
 }
@@ -270,6 +271,7 @@ impl From<wadm::types::StatusType> for StatusType {
             wadm::types::StatusType::Reconciling => StatusType::Reconciling,
             wadm::types::StatusType::Deployed => StatusType::Deployed,
             wadm::types::StatusType::Failed => StatusType::Failed,
+            wadm::types::StatusType::Waiting => StatusType::Waiting,
         }
     }
 }
