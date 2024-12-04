e4344426473337222f25e810c3166ca32af5bad2
Taylor Thomas <taylor@cosmonic.com>
2024-12-04 11:36:43 -0500

    chore: Polishes up flake with a few more clarifying comments
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
-  description = "Build a cargo workspace";
+  nixConfig.extra-substituters =
+    [ "https://wasmcloud.cachix.org" "https://crane.cachix.org" ];
+  nixConfig.extra-trusted-public-keys = [
+    "wasmcloud.cachix.org-1:9gRBzsKh+x2HbVVspreFg/6iFRiD4aOcUQfXVDl3hiM="
+    "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
+  ];
+
+  description = "A flake for building and running wadm";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/24.11-beta";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
 
     crane.url = "github:ipetkov/crane";
 
@@ -42,11 +49,10 @@
           buildInputs = [
             # Add additional build inputs here
           ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
+            # Additional darwin specific inputs can be set here if needed
           ];
 
-          # Additional environment variables can be set directly
+          # Additional environment variables can be set directly here if needed
           # MY_CUSTOM_VAR = "some value";
         };
 
@@ -71,89 +77,76 @@
 
         cargoLock = craneLib.writeTOML "Cargo.lock" filteredLockFile;
 
-        # Build *just* the cargo dependencies (of the entire workspace),
-        # so we can reuse all of that work (e.g. via cachix) when running in CI
-        # It is *highly* recommended to use something like cargo-hakari to avoid
-        # cache misses when building individual top-level-crates
+        # Build *just* the cargo dependencies (of the entire workspace), but we don't want to build
+        # any of the other things in the crate to avoid rebuilding things in the dependencies when
+        # we change workspace crate dependencies
         cargoArtifacts = let
-          commonArgs' = removeAttrs commonArgs ["src"];
+          commonArgs' = removeAttrs commonArgs [ "src" ];
 
           # Get the manifest file for filtering
           rawManifestFile = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 
           # Filter out the workspace members from manifest
-          filteredManifestFile = with lib; let
-            filterWadmAttrs = filterAttrs (name: _: !strings.hasPrefix "wadm" name);
+          filteredManifestFile = with lib;
+            let
+              filterWadmAttrs =
+                filterAttrs (name: _: !strings.hasPrefix "wadm" name);
 
-            workspace = removeAttrs rawManifestFile.workspace ["members"];
-          in
-            rawManifestFile
-            // {
-              workspace =
-                workspace
-                // {
-                  dependencies = filterWadmAttrs workspace.dependencies;
-                  package =
-                    workspace.package
-                    // {
-                      # pin version to avoid rebuilds on bumps
-                      version = "0.0.0";
-                    };
+              workspace = removeAttrs rawManifestFile.workspace [ "members" ];
+            in rawManifestFile // {
+              workspace = workspace // {
+                dependencies = filterWadmAttrs workspace.dependencies;
+                package = workspace.package // {
+                  # pin version to avoid rebuilds on bumps
+                  version = "0.0.0";
                 };
+              };
 
               dependencies = filterWadmAttrs rawManifestFile.dependencies;
 
-              dev-dependencies = filterWadmAttrs rawManifestFile.dev-dependencies;
+              dev-dependencies =
+                filterWadmAttrs rawManifestFile.dev-dependencies;
 
-              build-dependencies = filterWadmAttrs rawManifestFile.build-dependencies;
+              build-dependencies =
+                filterWadmAttrs rawManifestFile.build-dependencies;
             };
 
           cargoToml = craneLib.writeTOML "Cargo.toml" filteredManifestFile;
 
           dummySrc = craneLib.mkDummySrc {
-            src = pkgs.runCommand "wadm-dummy-src" {} ''
+            src = pkgs.runCommand "wadm-dummy-src" { } ''
               mkdir -p $out
               cp --recursive --no-preserve=mode,ownership ${src}/. -t $out
               cp ${cargoToml} $out/Cargo.toml
             '';
           };
 
-          args =
-            commonArgs'
-            // {
-              inherit
-                cargoLock
-                cargoToml
-                cargoVendorDir
-                dummySrc
-                ;
+          args = commonArgs' // {
+            inherit cargoLock cargoToml cargoVendorDir dummySrc;
 
-              cargoExtraArgs = ""; # disable `--locked` passed by default by crane
-            };
-        in
-          craneLib.buildDepsOnly args;
+            cargoExtraArgs = ""; # disable `--locked` passed by default by crane
+          };
+        in craneLib.buildDepsOnly args;
 
         individualCrateArgs = commonArgs // {
           inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
-          # TODO(thomastaylor312) We run unit tests here and e2e tests later. The nextest step
+          # TODO(thomastaylor312) We run unit tests here and e2e tests externally. The nextest step
           # wasn't letting me pass in the fileset
           doCheck = true;
         };
 
-        fileSetForCrate = crate:
-          lib.fileset.toSource {
-            root = ./.;
-            fileset = lib.fileset.unions [
-              ./Cargo.toml
-              # ./Cargo.lock
-              ./tests
-              ./oam
-              (craneLib.fileset.commonCargoSources ./crates/wadm)
-              (craneLib.fileset.commonCargoSources ./crates/wadm-client)
-              (craneLib.fileset.commonCargoSources ./crates/wadm-types)
-              (craneLib.fileset.commonCargoSources crate)
-            ];
-          };
+        fileSetForCrate = lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            ./tests
+            ./oam
+            (craneLib.fileset.commonCargoSources ./crates/wadm)
+            (craneLib.fileset.commonCargoSources ./crates/wadm-client)
+            (craneLib.fileset.commonCargoSources ./crates/wadm-types)
+          ];
+        };
 
         # Build the top-level crates of the workspace as individual derivations.
         # This allows consumers to only depend on (and build) only what they need.
@@ -163,32 +156,32 @@
         # Note that the cargo workspace must define `workspace.members` using wildcards,
         # otherwise, omitting a crate (like we do below) will result in errors since
         # cargo won't be able to find the sources for all members.
+        # TODO(thomastaylor312) I tried using `doInstallCargoArtifacts` and passing in things to the
+        # next derivations as the `cargoArtifacts`, but that ended up always building things twice
+        # rather than caching. We should look into it more and see if there's a way to make it work.
         wadm-lib = craneLib.cargoBuild (individualCrateArgs // {
+          inherit cargoArtifacts;
           pname = "wadm";
           cargoExtraArgs = "-p wadm";
-          src = fileSetForCrate ./crates/wadm;
-          cargoArtifacts = wadm-types;
-          doInstallCargoArtifacts = true;
+          src = fileSetForCrate;
         });
         wadm = craneLib.buildPackage (individualCrateArgs // {
+          inherit cargoArtifacts;
           pname = "wadm-cli";
           cargoExtraArgs = "--bin wadm";
-          src = fileSetForCrate ./.;
-          cargoArtifacts = wadm-lib;
+          src = fileSetForCrate;
         });
         wadm-client = craneLib.cargoBuild (individualCrateArgs // {
+          inherit cargoArtifacts;
           pname = "wadm-client";
           cargoExtraArgs = "-p wadm-client";
-          src = fileSetForCrate ./crates/wadm-client;
-          cargoArtifacts = wadm-types;
-          doInstallCargoArtifacts = true;
+          src = fileSetForCrate;
         });
         wadm-types = craneLib.cargoBuild (individualCrateArgs // {
           inherit cargoArtifacts;
           pname = "wadm-types";
           cargoExtraArgs = "-p wadm-types";
-          src = fileSetForCrate ./crates/wadm-types;
-          doInstallCargoArtifacts = true;
+          src = fileSetForCrate;
         });
       in {
         checks = {
@@ -202,13 +195,12 @@
           # we can block the CI if there are issues here, but not
           # prevent downstream consumers from building our crate by itself.
           workspace-clippy = craneLib.cargoClippy (commonArgs // {
-            # TODO: This will get better if we do the dummy build step mentioned above
-            cargoArtifacts = wadm-lib;
+            inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           });
 
           workspace-doc =
-            craneLib.cargoDoc (commonArgs // { cargoArtifacts = wadm-lib; });
+            craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
 
           # Check formatting
           workspace-fmt = craneLib.cargoFmt { inherit src; };
@@ -221,6 +213,13 @@
           #   inherit src;
           # };
 
+          # TODO: the wadm e2e tests use docker compose and things like `wash up` to test things 
+          # (which accesses network currently). We would need to fix those tests to do something
+          # else to work properly. The low hanging fruit here would be to use the built artifact
+          # in the e2e tests so we can output those binaries from the nix build and then just
+          # run the tests from a separate repo. We could also do something like outputting the
+          # prebuilt artifacts out into the current directory to save on build time. But that is
+          # for later us to figure out
           runE2ETests = pkgs.runCommand "e2e-tests" {
             nativeBuildInputs = with pkgs;
               [
@@ -228,18 +227,16 @@
                 # wasmcloud.wasmcloud
               ];
           } ''
-
-            # TODO: run test setup with wash and figure out how to expose it in the flake
             touch $out
           '';
         };
 
         packages = {
-          inherit wadm wadm-client wadm-types wadm-lib cargoArtifacts cargoLock;
+          inherit wadm wadm-client wadm-types wadm-lib;
           default = wadm;
         } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
           workspace-llvm-coverage = craneLibLLvmTools.cargoLlvmCov
-            (commonArgs // { cargoArtifacts = wadm-lib; });
+            (commonArgs // { inherit cargoArtifacts; });
         };
 
         apps = {
