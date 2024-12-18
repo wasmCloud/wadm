6fc79d3c81d3b8a4e7cfe231cfdb4c91a8931031
Márk Kővári <kovarimarkofficial@gmail.com>
2024-12-18 17:43:15 +0100

    fix(typo): persistance to persistence
    Signed-off-by: Márk Kővári <kovarimarkofficial@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -191,7 +191,7 @@ struct Args {
         default_value_t = StreamPersistence::File,
         hide = true
     )]
-    stream_persistance: StreamPersistence,
+    stream_persistence: StreamPersistence,
     /// Maximum bytes to keep for the command stream
     #[arg(
         long = "command-stream-max-bytes",
@@ -305,7 +305,7 @@ async fn main() -> anyhow::Result<()> {
                 .to_string(),
         ),
         args.max_event_stream_bytes,
-        args.stream_persistance.into(),
+        args.stream_persistence.into(),
     )
     .await?;
 
@@ -317,7 +317,7 @@ async fn main() -> anyhow::Result<()> {
         vec![DEFAULT_COMMANDS_TOPIC.to_owned()],
         Some("A stream that stores all commands for wadm".to_string()),
         args.max_command_stream_bytes,
-        args.stream_persistance.into(),
+        args.stream_persistence.into(),
     )
     .await?;
 
@@ -326,7 +326,7 @@ async fn main() -> anyhow::Result<()> {
         internal_stream_name(STATUS_STREAM_NAME),
         vec![DEFAULT_STATUS_TOPIC.to_owned()],
         args.max_status_stream_bytes,
-        args.stream_persistance.into(),
+        args.stream_persistence.into(),
     )
     .await?;
 
@@ -356,7 +356,7 @@ async fn main() -> anyhow::Result<()> {
                 .to_string(),
         ),
         args.max_wasmbus_event_stream_bytes,
-        args.stream_persistance.into(),
+        args.stream_persistence.into(),
     )
     .await?;
 
@@ -367,7 +367,7 @@ async fn main() -> anyhow::Result<()> {
         NOTIFY_STREAM_NAME.to_owned(),
         vec![format!("{WADM_NOTIFY_PREFIX}.*")],
         args.max_notify_stream_bytes,
-        args.stream_persistance.into(),
+        args.stream_persistence.into(),
     )
     .await?;
 
@@ -383,7 +383,7 @@ async fn main() -> anyhow::Result<()> {
                 .to_string(),
         ),
         args.max_event_consumer_stream_bytes,
-        args.stream_persistance.into(),
+        args.stream_persistence.into(),
     )
     .await?;
 
