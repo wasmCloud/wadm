11aa88b73fa385a12a6f41359e7c1e209751fccf
luk3ark <luk3ark@gmail.com>
2025-01-02 04:23:54 +0000

    feat(deps): removed default std feature and updated to target_family
    Signed-off-by: luk3ark <luk3ark@gmail.com>

--- crates/wadm-types/src/validation.rs
+++ crates/wadm-types/src/validation.rs
@@ -2,7 +2,7 @@
 //!
 
 use std::collections::{HashMap, HashSet};
-#[cfg(feature = "std")]
+#[cfg(not(target_family = "wasm"))]
 use std::path::Path;
 use std::sync::OnceLock;
 
@@ -274,7 +274,7 @@ impl ValidationOutput for Vec<ValidationFailure> {
 /// # Arguments
 ///
 /// * `path` - Path to the Manifest that will be read into memory and validated
-#[cfg(feature = "std")]
+#[cfg(not(target_family = "wasm"))]
 pub async fn validate_manifest_file(
     path: impl AsRef<Path>,
 ) -> Result<(Manifest, Vec<ValidationFailure>)> {
